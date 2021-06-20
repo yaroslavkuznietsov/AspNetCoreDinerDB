3355a1cb47602ac3bf10542818ee70b4be95eb4f
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-06-21 01:52:44 +0300

    First push. DataLibrary for WebApps Project

---
+++ DinerDB/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select * from dbo.Food)
+begin
+    insert into dbo.Food(Title, [Description], Price)
+    values ('Cheesburger Meal', 'A cheeseburger, fries and drink', 5.95),
+    ('Chili Dog Meal', 'Two chili dogs, fries and drink', 4.15),
+    ('Vegan Meal', 'A large salad and water', 1.95);
+end
